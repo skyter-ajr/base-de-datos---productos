6d0850d2b97d3d3d9a20ae116728a4b51b8384a0
Antonio Rudas Sanchez <62669845+skyter-ajr@users.noreply.github.com>
2021-01-12 11:28:56 -0500

    Update database1.sql

--- database1.sql
+++ database1.sql
@@ -1,5 +1,16 @@
-create table persona(
-    pk_id int (11),
-    nombre varchar (30),
-    
-)
+create database empresa
+
+use empresa
+
+-- crear una tabla
+create table inventario(
+    pk_id int (5) not null auto_increment,
+    producto varchar (30),
+    cantidad_kg int (3),
+    codigo_producto varchar (8),
+    primary key (pk_id)
+) engine=innodb;
+
+insert into inventario (producto, cantidad_kg, codigo_producto) values(1, 'harina', 10, 'h1we8hi23');
+
+select * from inventario;

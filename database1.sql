a6175d79d210725828916c410e400930db4cada3
Antonio Rudas Sanchez <62669845+skyter-ajr@users.noreply.github.com>
2021-01-13 07:21:09 -0500

    Update database1.sql

--- database1.sql
+++ database1.sql
@@ -3,14 +3,6 @@ create database empresa
 use empresa
 
 -- crear una tabla
-create table inventario(
-    pk_id int (5) not null auto_increment,
-    producto varchar (30),
-    cantidad_kg int (3),
-    codigo_producto varchar (8),
-    primary key (pk_id)
-) engine=innodb;
-insert into inventario (producto, cantidad_kg, codigo_producto) values('harina', 10, 'h1we8hi23');
 
 create table cliente(
     sp_id int (5) not null auto_increment,
@@ -19,7 +11,18 @@ create table cliente(
     cedula int (10),
     primary key (sp_id)
 )engine=innodb;
-insert into cliente values('*****', 40, 2219841274)
+insert into cliente(nombre, edad, cedula) values('*****', 40, 2219841274);
+
+create table inventario(
+    pk_id int (5) not null auto_increment,
+    producto varchar (30),
+    cantidad_kg int (3),
+    codigo_producto varchar (8),
+    fk_cliente int (5) not null,
+    primary key (pk_id),
+    foreign key (fk_cliente) references cliente (pk_id),
+) engine=innodb;
+insert into inventario (producto, cantidad_kg, codigo_producto) values('harina', 10, 'h1we8hi23');
 
 select * from inventario;
 select * from cliente;

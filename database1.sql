0697c5f973f0c6677c6bf16d3048ba1d22554ba8
Antonio Rudas Sanchez <62669845+skyter-ajr@users.noreply.github.com>
2021-01-13 07:13:07 -0500

    Update database1.sql

--- database1.sql
+++ database1.sql
@@ -10,10 +10,8 @@ create table inventario(
     codigo_producto varchar (8),
     primary key (pk_id)
 ) engine=innodb;
+insert into inventario (producto, cantidad_kg, codigo_producto) values('harina', 10, 'h1we8hi23');
 
-insert into inventario (producto, cantidad_kg, codigo_producto) values(1, 'harina', 10, 'h1we8hi23');
-
-select * from inventario;
 create table cliente(
     sp_id int (5) not null auto_increment,
     nombre varchar (25),
@@ -21,4 +19,7 @@ create table cliente(
     cedula int (10),
     primary key (sp_id)
 )engine=innodb;
+insert into cliente values('*****', 40, 2219841274)
+
+select * from inventario;
 select * from cliente;
